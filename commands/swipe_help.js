9043afdacf29636017ddcd768904fcf75017c3a4
Gerard Montemayor <me@grdmnt.com>
2021-03-20 19:26:14 +0800

    Update swipe_help

--- commands/swipe_help.js
+++ commands/swipe_help.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "Returns a google sheet link of the whale leaderboard",
   execute(message, args) {
     message.channel.send(
-      "Use the format ?swipe {UID} {ITEM} where ITEM can be: \nBP1K\nBP500\nWelkin\nGC5000\nGC2500\nGC1500\nGC750\nGC250\nGC50"
+      "Use the format ?swipe {ITEM} where ITEM can be: \nBP1K\nBP500\nWelkin\nGC5000\nGC2500\nGC1500\nGC750\nGC250\nGC50"
     );
   },
 };
